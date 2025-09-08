0505f51aec845db4764617d66c86d7ad7b301948
SamSchiavone <sam.schiavone@gmail.com>
2025-09-08 06:51:14 +0000

    Deploying to gh-pages from @ SamSchiavone/SamSchiavone.github.io@6f2a7346dafb5e86b7cc69d7771cac5be8dc53b8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,14 +25,14 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
